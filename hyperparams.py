3736d72538ae981e69773627ea3693dcf8ae44f4
bamtercelboo <bamtercelboo@163.com>
2017-11-21 10:31:10 +0800

    update demo of first commit

---
+++ hyperparams.py
@@ -0,0 +1,86 @@
+import torch
+import random
+torch.manual_seed(121)
+random.seed(121)
+# random seed
+seed_num = 233
+
+
+class Hyperparams():
+    def __init__(self):
+
+        # data path
+        self.train_path = "./pos_test_data/train.ctb60.pos-1.hwc"
+        self.dev_path = "./pos_test_data/dev.ctb60.pos-1.hwc"
+        self.test_path = "./pos_test_data/test.ctb60.pos-1.hwc"
+        # self.train_path = "./pos_test_data/train.ctb60.pos.hwc"
+        # self.dev_path = "./pos_test_data/dev.ctb60.pos.hwc"
+        # self.test_path = "./pos_test_data/test.ctb60.pos.hwc"
+        # self.train_path = "./posdata/train.ctb60.pos.hwc"
+        # self.dev_path = "./posdata/dev.ctb60.pos.hwc"
+        # self.test_path = "./posdata/test.ctb60.pos.hwc"
+
+        self.learning_rate = 0.001
+        # self.learning_rate_decay = 0.9   # value is 1 means not change lr
+        self.learning_rate_decay = 1   # value is 1 means not change lr
+        self.epochs = 2000
+        self.batch_size = 16
+        self.log_interval = 1
+        self.dev_interval = 4000000
+        self.test_interval = 4000000
+        self.save_dir = "snapshot"
+        self.shuffle = False
+        self.epochs_shuffle = True
+        self.dropout = 0.25
+        self.dropout_embed = 0.25
+        self.max_norm = None
+        self.clip_max_norm = 10
+        self.static = False
+        # model
+        self.Wordlstm = True
+        self.LSTM = False
+        self.BiLSTM_1 = False
+        # select optim algorhtim to train
+        self.Adam = True
+        self.SGD = False
+        self.Adadelta = False
+        self.optim_momentum_value = 0.9
+        # min freq to include during built the vocab, default is 1
+        self.min_freq = 1
+        self.word_min_freq = 1
+        self.char_min_freq = 2
+        self.bichar_min_freq = 2
+        # word_Embedding
+        self.word_Embedding = True
+        self.word_Embedding_Path = "./word2vec/glove.sentiment.conj.pretrained.txt"
+        self.fix_Embedding = False
+        self.embed_dim = 300
+        self.embed_char_dim = 200
+        self.embed_bichar_dim = 200
+        # word_Embedding_Path = "./word2vec/glove.840B.300d.handled.Twitter.txt"
+        self.char_Embedding = False
+        self.char_Embedding_path = "./word_embedding/char.vec"
+        self.bichar_Embedding = False
+        self.bichar_Embedding_Path = "./word_embedding/bichar-small.vec"
+        # self.bichar_Embedding_Path = "./word_embedding/convert_bichar.txt"
+        # self.pos_num = None
+        self.pos_dim = 100
+
+        self.rnn_hidden_dim = 200
+        self.hidden_size = 200
+        self.rnn_num_layers = 1
+        self.gpu_device = 0
+        self.use_cuda = False
+        self.snapshot = None
+        self.num_threads = 1
+        # whether to init w
+        self.init_weight = True
+        self.init_weight_value = 6.0
+        # L2 weight_decay
+        self.weight_decay = 1e-8  # default value is zero in Adam SGD
+        # self.weight_decay = 0   # default value is zero in Adam SGD
+        # whether to delete the model after test acc so that to save space
+        self.rm_model = True
+
+
+
---
+++ loaddata/Load_external_word_embedding.py
@@ -0,0 +1,160 @@
+# coding=utf-8
+import torch
+import random
+import collections
+import numpy as np
+import hyperparams as hy
+torch.manual_seed(hy.seed_num)
+random.seed(hy.seed_num)
+
+"""
+    load external word embedding
+"""
+
+
+class Word_Embedding():
+    def __init__(self):
+        print("loading external word embedding")
+        self.test = 0
+
+    def test(self):
+        print(self.test)
+
+    # load word embedding
+    def load_my_vecs(self, path, vocab, freqs, k=None):
+        # word_vecs = {}
+        word_vecs = collections.OrderedDict()
+        with open(path, encoding="utf-8") as f:
+            iov_count = 0
+            words = []
+            lines = f.readlines()[1:]
+            for line in lines:
+                values = line.split(" ")
+                word = values[0]
+                words.append(word)
+
+            for vocab_word in vocab:
+                for words_index, words_word in enumerate(words):
+                    if vocab_word == words_word:
+                        iov_count += 1
+                        values = lines[words_index].split(" ")
+                        vector = []
+                        for count, val in enumerate(values):
+                            if count == 0:
+                                continue
+                            if count <= k:
+                                vector.append(float(val))
+                        word_vecs[vocab_word] = vector
+                        break
+            print("iov count {}".format(iov_count))
+            print("oov count {}".format(len(vocab) - iov_count))
+        return word_vecs
+
+    def load_my_vecs_111(self, path, vocab, freqs, k=None):
+        word_vecs = {}
+        with open(path, encoding="utf-8") as f:
+            count = 0
+            lines = f.readlines()[1:]
+            for line in lines:
+                values = line.split(" ")
+                word = values[0]
+                # word = word.lower()
+                # if word in vocab and freqs[word] != 1:  # whether to judge if in vocab
+                count += 1
+                if word in vocab:  # whether to judge if in vocab
+                    # if word in vocab:  # whether to judge if in vocab
+                    #     if count % 5 == 0 and freqs[word] == 1:
+                    #         continue
+                    vector = []
+                    for count, val in enumerate(values):
+                        if count == 0:
+                            continue
+                        if count <= k:
+                            vector.append(float(val))
+                    word_vecs[word] = vector
+        return word_vecs
+
+    # solve unknown by avg word embedding
+    def add_unknown_words_by_avg(self, word_vecs, vocab, k=100):
+        # solve unknown words inplaced by zero list
+        word_vecs_numpy = []
+        for word in vocab:
+            if word in word_vecs:
+                word_vecs_numpy.append(word_vecs[word])
+        # print(len(word_vecs_numpy))
+        col = []
+        for i in range(k):
+            sum = 0.0
+            # for j in range(int(len(word_vecs_numpy) / 4)):
+            for j in range(int(len(word_vecs_numpy))):
+                sum += word_vecs_numpy[j][i]
+                sum = round(sum, 6)
+            col.append(sum)
+        zero = []
+        for m in range(k):
+            # avg = col[m] / (len(col) * 5)
+            avg = col[m] / (len(word_vecs_numpy))
+            avg = round(avg, 6)
+            zero.append(float(avg))
+
+        # print("zero", zero)
+
+        list_word2vec = []
+        oov = 0
+        iov = 0
+        for word in vocab:
+            if word not in word_vecs:
+                # word_vecs[word] = np.random.uniform(-0.25, 0.25, k).tolist()
+                # word_vecs[word] = [0.0] * k
+                oov += 1
+                word_vecs[word] = zero
+                list_word2vec.append(word_vecs[word])
+            else:
+                iov += 1
+                list_word2vec.append(word_vecs[word])
+        print("oov count", oov)
+        print("iov count", iov)
+        return list_word2vec
+
+    # solve unknown word by uniform(-0.25,0.25)
+    def add_unknown_words_by_uniform(self, word_vecs, vocab, k=100):
+        list_word2vec = []
+        oov = 0
+        iov = 0
+        # uniform = np.random.uniform(-0.25, 0.25, k).round(6).tolist()
+        for word in vocab:
+            if word not in word_vecs:
+                oov += 1
+                word_vecs[word] = np.random.uniform(-0.25, 0.25, k).round(6).tolist()
+                # word_vecs[word] = np.random.uniform(-0.1, 0.1, k).round(6).tolist()
+                # word_vecs[word] = uniform
+                list_word2vec.append(word_vecs[word])
+            else:
+                iov += 1
+                list_word2vec.append(word_vecs[word])
+        print("oov count", oov)
+        print("iov count", iov)
+        print("all words", oov + iov)
+        return list_word2vec
+
+    # load word embedding
+    def load_my_vecs_freq1(self, path, vocab, freqs, pro):
+        word_vecs = {}
+        with open(path, encoding="utf-8") as f:
+            freq = 0
+            lines = f.readlines()[1:]
+            for line in lines:
+                values = line.split(" ")
+                word = values[0]
+                if word in vocab:  # whehter to judge if in vocab
+                    if freqs[word] == 1:
+                        a = np.random.uniform(0, 1, 1).round(2)
+                        if pro < a:
+                            continue
+                    vector = []
+                    for count, val in enumerate(values):
+                        if count == 0:
+                            continue
+                        vector.append(float(val))
+                    word_vecs[word] = vector
+        return word_vecs
